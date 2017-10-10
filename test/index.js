c3768adc774a8bf618fc674e176c5614a0745287
Stephan Georg <s.georg@nearest.place>
2017-10-10 10:27:06 +0200

    Updated test

--- test/index.js
+++ test/index.js
@@ -9,8 +9,18 @@ describe('Query DNS Server', () => {
       .then((result) => {
         // console.log(result);
         expect(result).to.be.an('object')
-          .and.to.have.property('answer')
+          .and.to.have.property('question')
           .and.to.be.an('array');
+        expect(result).to.have.property('answer')
+          .and.to.be.an('array');
+        expect(result).to.have.property('time')
+          .and.to.be.an('number');
+        expect(result).to.have.property('server')
+          .and.to.be.an('string');
+        expect(result).to.have.property('datetime')
+          .and.to.be.an('string');
+        expect(result).to.have.property('size')
+          .and.to.be.an('number');
         done();
       })
       .catch((err) => {
